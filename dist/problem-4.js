9c7111cf69c6ea2c140555a24f1e015c1b3505c4
sanjidbari <sanjidbari.com>
2024-11-08 22:50:31 -0500

    PS 4 done

--- dist/problem-4.js
+++ dist/problem-4.js
@@ -2,16 +2,17 @@
 //Problem 4
 function calculateShapeArea(shape) {
     if ('radius' in shape) {
-        return Math.PI * Math.pow(shape.radius, 2); // Area of a circle: π * r^2
+        return Math.PI * Math.pow(shape.radius, 2.0);
     }
     else {
-        return shape.width * shape.height; // Area of a rectangle: width * height
+        return shape.width * shape.height;
     }
 }
-var circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+var circleArea = calculateShapeArea({ shape: "circle", radius: 5 }).toFixed(2);
 console.log(circleArea);
 var rectangleArea = calculateShapeArea({
     shape: "rectangle",
     width: 4,
     height: 6,
 });
+console.log(rectangleArea);
